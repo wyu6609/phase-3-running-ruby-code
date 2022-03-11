27cb9e2c5920a2b7a0727c563ffd1ea188543d78
William Yu <wy427@nyu.edu>
2022-03-11 18:05:14 -0500

    completed

---
+++ app.rb
@@ -0,0 +1,14 @@
+# app.rb
+puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# puts [1,2,3]
+p[1,2,3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
